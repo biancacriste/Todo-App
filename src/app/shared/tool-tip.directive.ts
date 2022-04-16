d08c7e52324951d0bec709722575910d8b094a98
bianca.criste <criste.bianca@gmail.com>
2022-04-16 22:50:20 +0300

    commit local

---
+++ src/app/shared/tool-tip.directive.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import tippy from 'tippy.js';
+
+@Directive({
+  selector: '[appToolTip]'
+})
+export class ToolTipDirective implements AfterViewInit, OnChanges {
+
+  @Input('appToolTip')
+  tooltipContent?: string;
+
+   public tippyInstance: any;
+
+  constructor(private elRef: ElementRef) { }
+
+  ngAfterViewInit(): void {
+    this.tippyInstance = tippy(this.elRef.nativeElement, {
+      content: this.tooltipContent
+    })
+  }
+
+  ngOnChanges (changes: SimpleChanges) {
+
+    if (changes['toolTipContent']) { //input content has changed
+      this.updateToolTipContent()
+    }
+  }
+
+  updateToolTipContent () {
+    if (this.tippyInstance) {
+      this.tippyInstance.setContent(this.tooltipContent)
+    }
+  }
+
+}
+
